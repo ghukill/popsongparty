8ced5f1f69dba6ce469eceb37bd13b1af57d9102
root <root@mitten.(none)>
2014-07-03 14:50:47 +0000

    scoring, making waves

--- index.php
+++ index.php
@@ -1,101 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="images/favicon.ico">
-
-    <title>Quiz: Pop Song or Party?</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="inc/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/stylesheet.css" rel="stylesheet">    
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-  <body>
-
-    <!-- Fixed navbar -->
-    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Quiz: Pop Song or Party?</a>
-        </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">            
-            <li class="active"><a href="#about">About</a></li>
-            <li><a href="#contact">The Quiz</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Jump to... <span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
+<?php include "inc/header.php" ?>
 
     <!-- Begin page content -->
     <div class="container">
       
       <div class="row">
-        <div id="quiz_logo" class="col-md-8">
+        <div id="quiz_logo" class="col-md-6">
           <img src="img/quiz/quiz_logo.jpg" alt="quiz_logo" class="img-rounded img-responsive" >
         </div>
-      </div>
-
-      <div class="row">
-        <div class="col-md-12">
-          <p>Welcome to the quiz, "Pop Song or Party" whether nothing is clear!  For each question, you will be presented with a name, and the option of either "political party" or "pop song".  Choose wisely and have fun.
+        <div id="splash_text" class="text col-md-6">
+          <p>Welcome to the quiz.  For each question, you will be presented with a name, and the option of either "political party" or "pop song".  Choose wisely and have fun.</p>
+          <p>(more text...) Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
         </div>
-      </div>
-
-      <div class="row">
-        <div class="col-md-6">
-          <img src="img/quiz/intro_1.jpg" alt="quiz_logo" class="img-rounded img-responsive" >          
-        </div>
-        <div class="col-md-6">
-          <img src="img/quiz/intro_2.jpg" alt="quiz_logo" class="img-rounded img-responsive" >          
-        </div>
-      </div>
-      
+      </div>      
       
     </div>
 
-    <div class="footer">
-      <div class="container">
-        <p class="text-muted">Brought to you by...</p>
-      </div>
-    </div>
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="inc/bootstrap/js/bootstrap.min.js"></script>
-  </body>
-</html>
+<?php include "inc/footer.php" ?>
---
+++ js/main.js
@@ -0,0 +1,60 @@
+
+var answers = {
+	q1:"par",
+	q2:"pop",
+	q3:"par",
+	q4:"both",
+	q5:"par",
+	q6:"par",
+	q7:"pop",
+	q8:"pop",
+	q9:"pop",
+	q10:"par",
+	q11:"pop",
+	q12:"par",
+	q13:"both"	
+}
+
+// prime score
+if (localStorage.getItem('quiz_score') == null){
+	localStorage.setItem('quiz_score',0);
+}
+
+// click listeners
+function showAns(choice,qnum){
+	console.log(choice,qnum);
+
+	// show answer
+	$("#quiz_canvas").hide();
+	$("#"+choice+"_ans").fadeIn();
+
+	// check and store score
+	if (answers[qnum] == choice || answers[qnum] == "both"){
+		console.log("winner winner, chicken dinner!");
+		var nscore =  parseInt(localStorage.getItem('quiz_score')) + 1;
+		localStorage.setItem('quiz_score',nscore);
+	}
+	else {
+		console.log("so sorry... score remains the same");
+	}
+
+}
+
+function getScore(){
+	return localStorage.getItem('quiz_score');
+}
+
+function resetScore(){
+	localStorage.setItem('quiz_score',0);
+}
+
+// listener that resets score when hitting any navigation
+// requires straight through playing to get score
+$(document).ready(function(){
+	$(".navbar").click(function(){
+		resetScore();
+	})
+})
+
+
+
