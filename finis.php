362b64b9f14741911827504ddaf2e3d53239d7df
root <root@mitten.(none)>
2014-12-04 21:06:46 +0000

    changes before movement

--- finis.php
+++ finis.php
@@ -1,40 +1,20 @@
 <?php include "inc/header.php" ?>
 
     <!-- Begin page content -->
-    <div class="container">
-      
-      <!-- <div class="row">
-
-        <div class="col-md-6 left_finis">
-          <img src="img/quiz/quiz_logo.jpg" alt="quiz_logo" class="img-rounded img-responsive">
-        </div>
-
-        <div class="text col-md-6 text-center" id="score_box">
-          
-          <h1 >Thanks for playing!</h1>
-          <h2>Your score: <span id="score"></span> / <?php echo count($q_phrases); ?></h2>
-          <script type="text/javascript">
-            var score = getScore();
-            $("#score").html(score);
-          </script>          
-
-        </div>
-
-      </div>   -->    
+    <div class="container">  
       <div class="row text-center">       
 
         <div class="text col-md-12" id="score_box">
           
-          <h2>Thanks for playing <a href="./index.php">Party or Popsong</a>!  Your score is: <span id="score"></span> / <?php echo count($q_phrases); ?></h2>
+          <h1>Thanks for playing <a href="./index.php">Party or Popsong</a>!  Your score is: <span id="score"></span> / <?php echo count($q_phrases); ?></h1>
           <script type="text/javascript">
             var score = getScore();
             $("#score").html(score);
           </script>
-          <h3>But before you go, there are three points worth making, two of them frivolous, one serious.</h3>          
+          <h4>But before you go, there are three points worth making, two of them frivolous, one serious.</h4>
         </div>
 
       </div>  
-      <hr>
       <?php include "inc/about_text.php" ?>
       
     </div>
--- inc/about_text.php
+++ inc/about_text.php
@@ -5,38 +5,38 @@
     <!-- images -->
     <div style="margin-top:30px;" class="row">
 
-    <div class="col-md-6">
-      <p>1. If you’re writing a Eurovision song, look to the European Parliament for a great title.</p><br>
-      <img src="img/quiz/pop_ideas.jpg" alt="quiz_logo" class="img-rounded img-responsive">
+    <div class="col-md-6">      
+      <img src="img/quiz/pop_ideas.jpg" alt="quiz_logo" class="points_images img-rounded img-responsive">
       <br>
-      <p>Need more? See the <a href="http://politicaldatayearbook.com">Political Data Yearbook</a> where we have tried to provide a complete list. And don’t stop there.  Political party programs are a great source of lyrics (To take one random example: “Create an earthquake/It’s time to decide/We don’t need to wait/Tell them what you think/ It’s time for the truth.”  (<a href="http://bit.ly/ukip2014">UK Independence Party Manifesto, 2014</a>)</p>
+      <p><span class="points">One:</span> If you’re writing a Eurovision song, look to the European Parliament for a great title.</p><br>
+      <p align="justify">Need more? See the <a href="http://politicaldatayearbook.com">Political Data Yearbook</a> where we have tried to provide a complete list. And don’t stop there.  Political party programs are a great source of lyrics (To take one random example: “Create an earthquake/It’s time to decide/We don’t need to wait/Tell them what you think/ It’s time for the truth.”  (<a href="http://bit.ly/ukip2014">UK Independence Party Manifesto, 2014</a>)</p>
     </div>
 
     <div class="col-md-6">
-      <p>2. If you're founding a new party, recent Eurovision contests are a great source of names. </p><br>
-      <img src="img/quiz/par_ideas.jpg" alt="quiz_logo" class="img-rounded img-responsive">
+      <img src="img/quiz/par_ideas.jpg" alt="quiz_logo" class="points_images img-rounded img-responsive">
       <br>
-      <p>Of course with parties, picking the name is only a first step.  Then you need a logo that looks like it could launch an app on a smartphone (network diagrams, and circles are in) and hip color combinations like orchid and seaside green or gray and gold.  And you’ll need a party leader: see if you can find a well-respected television psychologist or fashion-model-turned-internet-entrepreneur.</p>
+      <p><span class="points">Two:</span> If you're founding a new party, recent Eurovision contests are a great source of names.</p><br>
+      <p align="justify">Of course with parties, picking the name is only a first step.  Then you need a logo that looks like it could launch an app on a smartphone (network diagrams, and circles are in) and hip color combinations like orchid and seaside green or gray and gold.  And you’ll need a party leader: see if you can find a well-respected television psychologist or fashion-model-turned-internet-entrepreneur.</p>
   	</div>
 
   </div>  
     
     <br>
     <br>
-    <p><strong>3. Finally, the serious point (I know, Ikea or Death didn't preach at you, but frankly politics is a bit more important than pressboard lampstands)</strong></p>
-    <p>This isn't just about party and song names but about how our civic life is changing. Organizations in all walks of life seem less-rooted, more flexible, more apt to change. And boundaries -- between genres, between substance and self-promotion, between fiction and reality -- seem ever more porous. This may just be an illusion -- every generation in modernity sees itself as moving faster than its predecessors -- but there are good reasons to think that things are changing (See Clay Shirkey's, "<a href="http://www.theguardian.com/books/2008/mar/22/society1">Here Comes Everybody</a>", and Moises Naim's, "<a href="http://bit.ly/naim-review-lse">The End of Power</a>").  If so, the questions facing of citizens becomes much more critical and difficult. How do we decide between compromised experience and unsullied novelty? How do we hold politicians accountable if they themselves really don't expect to be around by the next election?</p>
+    <p align="justify"><span class="points">Three:</span> Finally, the serious point (I know, Ikea or Death didn't preach at you, but frankly politics is a bit more important than pressboard lampstands)</p>
+    <p align="justify">This isn't just about party and song names but about how our civic life is changing. Organizations in all walks of life seem less-rooted, more flexible, more apt to change. And boundaries -- between genres, between substance and self-promotion, between fiction and reality -- seem ever more porous. This may just be an illusion -- every generation in modernity sees itself as moving faster than its predecessors -- but there are good reasons to think that things are changing (See Clay Shirkey's, "<a href="http://www.theguardian.com/books/2008/mar/22/society1">Here Comes Everybody</a>", and Moises Naim's, "<a href="http://bit.ly/naim-review-lse">The End of Power</a>").  If so, the questions facing of citizens becomes much more critical and difficult. How do we decide between compromised experience and unsullied novelty? How do we hold politicians accountable if they themselves really don't expect to be around by the next election?</p>
     <p>If you haven't stopped reading yet and are still interested, you can find more thoughts about the subject here: <a href="http://www.pozorblog.com/2014/07/perishable-goods/">http://www.pozorblog.com/2014/07/perishable-goods/</a>. See you in Vienna for Eurovision 2015.</p>
   
   </div>
 </div>
-<hr>
-<div style="margin-top:20px; margin-bottom:30px;" class="row">
+
+<div style="margin-top:60px; margin-bottom:30px;" class="row">
   <div class="col-md-12 text">                              
     <h4>Credits</h4>
 
-    <p>Thanks to Tim Haughton, Tereza Novotna, Zsolt Enyedi, Rainbow Murray, and Andreas Bagenholm for their encouragement.  Thanks to my wife Bridget and children Elena and Peter for the seeds of the idea.  And thanks to Graham Hukill of the Wayne State University Library for the fantastic work on implementing this rather odd idea.</p>
+    <p align="justify">Thanks to Tim Haughton, Tereza Novotna, Zsolt Enyedi, Rainbow Murray, and Andreas Bagenholm for their encouragement.  Thanks to my wife Bridget and children Elena and Peter for the seeds of the idea.  And thanks to Graham Hukill of the Wayne State University Library for the fantastic work on implementing this rather odd idea.</p>
     
-    <p>Kevin Deegan-Krause is an Associate Professor of Political Science at Wayne State University, co-editor of the European Consortium for Political Research Political Data Yearbook, and occasional blogger at <a href="http://www.pozorblog.com">http://www.pozorblog.com</a>.  He is proud of his obsession with political parties and slightly embarrassed by his obsession with the Eurovision Song Contest.  Or is it the other way around?</p>
+    <p align="justify">Kevin Deegan-Krause is an Associate Professor of Political Science at Wayne State University, co-editor of the European Consortium for Political Research Political Data Yearbook, and occasional blogger at <a href="http://www.pozorblog.com">http://www.pozorblog.com</a>.  He is proud of his obsession with political parties and slightly embarrassed by his obsession with the Eurovision Song Contest.  Or is it the other way around?</p>
     <br>
     <p><strong>Photo credits:</strong></p>
     <p>Image of Aliona Moon, Eurovision contestant from Moldova in 2013, <a href="http://www.eurovision.tv/save-files/resizes/18/04/9b/ae/67/de/2c/7c/8f/43/b2/f6/f5/fc/5f/85/TH222_12.jpg">http://www.eurovision.tv/save-files/resizes/18/04/9b/ae/67/de/2c/7c/8f/43/b2/f6/f5/fc/5f/85/TH222_12.jpg</a></p>
